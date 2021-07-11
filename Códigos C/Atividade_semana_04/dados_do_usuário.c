1b3a1ccf50d0e505e8b5fc4242fe30d9835c84a0
DKAT-DAVI <eudkatdavikalel@gmail.com>
2021-07-11 18:26:17 -0300

    Update dados_do_usuário.c

--- Códigos C/Atividade_semana_04/dados_do_usuário.c
+++ Códigos C/Atividade_semana_04/dados_do_usuário.c
@@ -1,9 +1,13 @@
 /*Questão 06 - Fazer um programa que leia o nome, endereço e telefone do usuário e mostre em seguida.*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
 
 int main()
 {
-    char nome[50];
+
+    char nome[50], estado[50];
     char rua[50], bairro[50],num[50], cel[50], idade[50], cep[50];
 
     printf("=-=-=-=-=-=-=-=-=-=-=-=CADASTRO=-=-=-=-=-=-=-=-=-=-=-=\n");
@@ -15,20 +19,34 @@ int main()
     printf("\nRUA: ");
     fgets(rua,50,stdin);
 
-    printf("\nBAIRRO: ");
-    fgets(bairro,50,stdin);
-
     printf("\nNUMERO: ");
     fgets(num,50,stdin);
+
+    printf("\nBAIRRO: ");
+    fgets(bairro,50,stdin);
     
     printf("\nCEP: ");
     fgets(cep,50,stdin);
+
+    printf("\nESTADO: ");
+    fgets(estado,50,stdin);
 
     printf("\nDigite seu numero de celular: ");
     fgets(cel,50,stdin);
 
     printf("\nDigite sua idade: ");
     fgets(idade,50,stdin);
+
+    printf("FINALIZANDO CADASTRO...");
+    Sleep(4000);
+
+    system("cls");
+    
+    system("color 1");
+
+    printf("=-=-=-=-=-=-=-=-=-=-=-=CADASTRADO=-=-=-=-=-=-=-=-=-=-=-=\n");
+
+    printf("\nNOME: %s\n\nENDEREcO\nRUA: %s\n\nNUMERO: %s\n\nBAIRRO: %s\n\nCEP: %s\n\nESTADO: %s\n\nCELULAR: %s\n\nIDADE: %s\n\n", nome, rua, bairro, num, cep, estado, cel, idade);
 
     system("PAUSE");
     return 0;
