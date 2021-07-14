31d82bd8d930931d19dcb25de005da36ce0fae67
DKAT-DAVI <eudkatdavikalel@gmail.com>
2021-07-14 20:44:14 -0300

    Finalizando calculadora

--- Códigos C/Atividades_semana_05/calculadora_básica.c
+++ Códigos C/Atividades_semana_05/calculadora_básica.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <conio.h>
 
 float soma(float x, float y)
 {   
     printf("%.1f", x + y);
+    return 0;
+}
+
+float sub(float x, float y)
+{   
+    printf("%.1f", x - y);
+    return 0;
+}
+
+float mult(float x, float y)
+{   
+    printf("%.1f", x * y);
+    return 0;
+}
+
+float div(float x, float y)
+{   
+    printf("%.1f", x / y);
     return 0;
 }
 
@@ -11,6 +30,8 @@ int main()
     float a, b;
     char oper = 'a';
     int pergunta = 1;
+
+    system("color 1");
 
     printf("\n==== CALCULADORA DE 2 NUMEROS ====\n");
     printf("|                                |\n");
@@ -22,15 +43,23 @@ int main()
     printf("\nCALCULAR OU SAIR? ");
     scanf("%d", &pergunta);
     
+    system("cls");
+
     while(pergunta > 2 || pergunta < 1){
             printf("OPCAO INVALIDA, TENTE NOVAMENTE!");
+            printf("\n\n================================\n");
+            printf("|                                |\n");
+            printf("|   FAZER CALCULO        ->  1   |\n");
+            printf("|   SAIR DA CALCULADORA  ->  2   |\n");
+            printf("|                                |\n");
+            printf("==================================\n");
             printf("\n\nCALCULAR OU SAIR? ");
             scanf("%d", &pergunta);
     }
 
     while(pergunta == 1){
 
-        printf("\n====================================\n");
+        printf("\n===== CALCULADORA DE 2 NUMEROS =====\n");
         printf("|      PARA SOMAR       ->  +      |\n");
         printf("|      PARA SUBTRAIR    ->  -      |\n");
         printf("|      PARA MULTIPLICAR ->  *      |\n");
@@ -42,22 +71,41 @@ int main()
         
         printf("\nDIGITE A OPERACAO: ");
         scanf("%s", &oper);
-        
-        printf("\nDIGITE O SEGUNDO NUMERO: ");
-        scanf("%f", &b);
+        while(oper != '+' && oper != '-' && oper != '*' && oper != '/'){
+            printf("\nOPERACAO INVALIDA, TENTE NOVAMENTE!");
+            printf("\nDIGITE A OPERACAO: ");
+            scanf("%s", &oper);
+        }
 
-        printf("\nRESULTADO: ");   
+        printf("\nDIGITE O SEGUNDO NUMERO: ");
+        scanf("%f", &b); 
 
         if(oper == '+')
         {
+            printf("\nRESULTADO: ");
             soma(a,b);
-        }else
+        }
+        else if(oper == '-')
         {
-            printf("\nOPERACAO INVALIDA, TENTE NOVAMENTE!");
+            printf("\nRESULTADO: ");
+            sub(a,b);
+        }
+        else if(oper == '*')
+        {
+            printf("\nRESULTADO: ");
+            mult(a,b);
+        }
+        else if(oper == '/')
+        {
+            printf("\nRESULTADO: ");
+            div(a,b);
         }
 
         printf("\n\nCALCULAR OUTRO NUMERO OU SAIR? ");
         scanf("%d", &pergunta);
+
+        system("cls");
     }
+    system("PAUSE");
     return 0;
 }
